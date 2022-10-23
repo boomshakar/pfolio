6d0cb288b3e26b576bd37edd77b8313a8322af9e
bomshakar <Hefehmzy@gmail.com>
2022-10-23 04:51:59 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+//
+$(document).ready(function () {
+	$("#fullpage").fullpage({
+		verticalCentered: false,
+		scrollingSpeed: 600,
+		autoScrolling: false,
+		css3: true,
+		navigation: true,
+		navigationPosition: "right",
+	});
+	const yrStart = 2018;
+	const currentYr = new Date().getFullYear();
+	const yearExp = currentYr - yrStart;
+	$("#yrExp").text(yearExp).replace(_, yearExp);
+
+	// CHange theme mode
+	$(".color-mode").click(function () {
+		$(".color-mode-icon").toggleClass("active");
+		$("body").toggleClass("dark-mode");
+	});
+});
+
+// wow
+$(function () {
+	new WOW().init();
+	$(".rotate").textrotator();
+});
